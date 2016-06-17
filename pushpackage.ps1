9b8841dbbfa8573a3d96f95ff6f1c89ee1aa8ee6
sreesanpd <sreesanpd@users.noreply.github.com>
2016-06-17 16:44:05 +0530

    you should use full path rather than using regex
    you should use full path rather than using regex while adding packagefilepath

---
+++ pushpackage.ps1
@@ -0,0 +1,45 @@
+# 1. Your Octopus URL
+$octopusUrl = "http://localhost";
+
+# 2. An API key, preferably for a Service Account (http://docs.octopusdeploy.com/display/OD/Service+Accounts)
+$apiKey = "API-XXXXXXXXXXXXXXXXXXXXXX"
+
+# 3. Path to the package file to upload. Make sure that you are using full path and not regex
+$packageFilePath = "C:\Temp\HelloWorldWebApp.2.1.0.0.nupkg";
+
+# 4. true to overwrite existing packages (Requires: BuiltInFeedAdminister permission)
+$replaceExisting = $true;
+
+$packageUrl = $octopusUrl + "/api/packages/raw?replace=" + $replaceExisting;
+
+Write-Host Uploading $packageFilePath to $packageUrl;
+
+$webRequest = [System.Net.HttpWebRequest]::Create($packageUrl);
+$webRequest.Accept = "application/json";
+$webRequest.ContentType = "application/json";
+$webRequest.Method = "POST";
+$webRequest.Headers["X-Octopus-ApiKey"] = $apiKey;
+
+
+$packageFileStream = new-object IO.FileStream $packageFilePath,'Open','Read','Read'
+
+    $boundary = "----------------------------" + [System.DateTime]::Now.Ticks.ToString("x");
+    $boundarybytes = [System.Text.Encoding]::ASCII.GetBytes("`r`n--" + $boundary + "`r`n")
+    $webRequest.ContentType = "multipart/form-data; boundary=" + $boundary;
+    $webRequest.GetRequestStream().Write($boundarybytes, 0, $boundarybytes.Length);
+
+    $header = "Content-Disposition: form-data; filename="""+ [System.IO.Path]::GetFileName($packageFilePath) +"""`r`nContent-Type: application/octet-stream`r`n`r`n";
+    $headerbytes = [System.Text.Encoding]::ASCII.GetBytes($header);
+    $webRequest.GetRequestStream().Write($headerbytes, 0, $headerbytes.Length);
+    $packageFileStream.CopyTo($webRequest.GetRequestStream());
+    $webRequest.GetRequestStream().Write($boundarybytes, 0, $boundarybytes.Length);
+    $webRequest.GetRequestStream().Flush();
+    $webRequest.GetRequestStream().Close();
+
+    $packageFileStream.Close();
+    $packageFileStream.Dispose();
+
+
+$webResponse = $webRequest.GetResponse();
+Write-Host $webResponse.StatusCode $webResponse.StatusDescription;  
+$webResponse.Dispose();
